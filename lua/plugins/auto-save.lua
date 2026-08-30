d71e2cf1e3149b0712fe60156055ea51848ef94e
Lorenzo Vecchio <lorenzo.vecchio@icloud.com>
2026-08-30 16:37:39 +0200

    update autosave plugin conf to work in insert mode and add lua file exception (to avoid bugs while editing neovim or omarchy configuration)

--- lua/plugins/auto-save.lua
+++ lua/plugins/auto-save.lua
@@ -1,9 +1,16 @@
 return {
   {
     "okuuva/auto-save.nvim",
-    event = { "InsertLeave", "TextChanged" },
+    event = { "InsertLeave", "TextChanged", "TextChangedI" },
     opts = {
       debounce_delay = 1000, -- save 1s after you stop typing
+      trigger_events = {
+        defer_save = { "InsertLeave", "TextChanged", "TextChangedI" },
+      },
+      condition = function(buf)
+        -- exception: never autosave lua files
+        return vim.bo[buf].filetype ~= "lua"
+      end,
       -- Optional: skip autosave for certain buffers
       -- condition = function(buf)
       --   return vim.bo[buf].filetype ~= "neo-tree"
